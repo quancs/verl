929b6562d9c175b3fa8e96f95764b20a4b69fad0
Lingfeng Wang <62014693+wlf-darkmatter@users.noreply.github.com>
2025-10-27 16:18:02 +0800

    上传客户megatron的MTP部分的返回逻辑
    k8s NPU完全对齐脚本上传
    
    k8s NPU完全对齐脚本上传
    
    .
    
    .
    
    GPUNPU脚本试用版
    
    规整GPU脚本
    
    上传mc2.yaml
    
    A2脚本
    
    fix: standardize job name casing and update runtime environment in scripts

---
+++ k8s/patch/apply_megatron.sh
@@ -0,0 +1,8 @@
+set -x
+
+
+\cp /home/code/verl-gpu/k8s/patch/megatron.patch/0.12.1/Megatron-LM/megatron/core/transformer/multi_token_prediction.py /opt/Megatron-LM/megatron/core/transformer/multi_token_prediction.py
+\cp /home/code/verl-gpu/k8s/patch/megatron.patch/0.12.1/Megatron-LM/megatron/core/transformer/dot_product_attention.py /opt/Megatron-LM/megatron/core/transformer/dot_product_attention.py
+echo -e "/033[32mApplied megatron MTP done./033[0m"
+
+set +x
