b53bbd5081610688ff64b08b0ee7b3d26d08a351
yangxiaojun0126 <zhangchunli19@huawei.com>
2025-10-28 19:08:25 +0800

    +NPU初步的启动脚本

---
+++ recipe/moonlight_NPU/main.sh
@@ -0,0 +1,94 @@
+####################################################
+# 本脚本用于，在多机训练时：
+# 1. 向其他节点，同步主节点修改后的数据
+# 2. 多机启动docker、启动训练脚本
+####################################################
+# 本脚本需要提前配置好ssh免密登录，即将master的公钥传到目标服务器，指令示例如下：
+# ssh-keygen
+# ssh-copy-id root@90.90.122.120
+####################################################
+# 脚本执行方式：
+# chmod +x main.sh
+# ./main.sh
+####################################################
+
+IPs=( # 除开master的其他节点IP
+    # "90.90.122.117"
+    "90.90.122.120"
+)
+
+# 需要同步的目录。目标目录与源目录不相同的内容会被删除
+DIRs=(
+    "/home/q00887491/models/Moonlight-16B-A3B"
+    "/home/q00887491/datasets/gsm8k"
+    "/home/q00887491/projects/wlf_darkmatter_verl"
+)
+
+# 映射到docker内的路径
+docker_v_dirs="-v ${DIRs[0]}:/data/models/Moonlight-16B-A3B:ro -v ${DIRs[1]}:/data/datasets/gsm8k:ro -v ${DIRs[2]}:/workspace/verl -v /home/q00887491/logs:/data/logs"
+
+# 启动脚本在docker内的路径
+docker_cmd_file="/workspace/verl/recipe/moonlight_NPU/start.sh"
+
+
+echo "#################  数据同步中  ####################"
+index=0
+for ip in ${IPs[*]}; do
+    echo -e "rsync to Node $index $ip"
+
+    for diri in ${DIRs[*]}; do
+        ssh root@$ip "mkdir -p $diri; exit" # 创建目录
+        echo -e "rsync -av --delete $diri/ root@$ip:$diri/"
+        rsync -av --delete $diri/ root@$ip:$diri/  # 同步数据，会删除目标目录中不一样的内容
+        echo -e "\n"
+    done
+
+    ((index++))  # 索引自增
+    echo -r "\n\n"
+done
+echo "#################  数据同步结束  ####################"
+
+echo "#################  训练启动中  ####################"
+# docker启动
+DOCKER_IMAGES_ID=verl_pt27:82RC2Commercial
+CONTAINER_NAME=test11
+
+DOCKER_START_CMD="docker run --name ${CONTAINER_NAME} -itd --net=host --shm-size=500g \
+    --privileged=true \
+    -w /root \
+   --device=/dev/davinci_manager \
+   --device=/dev/hisi_hdc \
+   --device /dev/devmm_svm \
+   --entrypoint=bash \
+   -v /usr/local/Ascend/driver:/usr/local/Ascend/driver:ro \
+   -v /usr/local/dcmi:/usr/local/dcmi:ro \
+   -v /usr/local/Ascend/firmware:/usr/local/Ascend/firmware:ro \
+   -v /etc/ascend_install.info:/etc/ascend_install.info:ro \
+   -v /usr/local/sbin:/usr/local/sbin:ro \
+   -v /etc/hccn.conf:/etc/hccn.conf:ro \
+   ${docker_v_dirs} \
+   ${DOCKER_IMAGES_ID}"
+
+DOCKER_RUN_CMD="docker exec ${CONTAINER_NAME} bash ${docker_cmd_file}"
+
+echo -e "$DOCKER_START_CMD"
+echo -e "$DOCKER_RUN_CMD"
+
+index=0
+for ip in ${IPs[*]}; do
+    echo -e "\nstart process on Node $index $ip"
+    ssh root@$ip "docker stop $CONTAINER_NAME; docker rm $CONTAINER_NAME; $DOCKER_START_CMD; $DOCKER_RUN_CMD; exit" & # +“&”后，后台起另外一个线程运行
+    ((index++))  # 索引自增
+    echo "################################################"
+done
+
+echo -e "\nstart process on Master Node"
+eval "docker stop $CONTAINER_NAME; docker rm $CONTAINER_NAME"
+eval $DOCKER_START_CMD
+eval $DOCKER_RUN_CMD
+
+# 等待后台任务完成
+wait
+
+echo "#################  训练结束  ####################"
+
---
+++ recipe/moonlight_NPU/start.sh
@@ -0,0 +1,116 @@
+pkill -9 python
+
+# 激活conda环境
+source /root/.bashrc
+conda activate verl_pt27_25rc2_0822daily
+ray stop --force
+rm -rf /tmp/ray
+
+export RAY_DEBUG=1  # 允许ray debug
+export RAY_DEBUG_POST_MORTEM=1
+export RAY_DEDUP_LOGS=1  # Ray 日志去重
+export HYDRA_FULL_ERROR=1
+
+#修改为当前需要跑的用例路径
+DEFAULT_SH="/workspace/verl/recipe/moonlight_NPU/run.sh"
+echo "Use $DEFAULT_SH"
+
+ulimit -n 32768
+
+export NNODES=2
+export NPUS_PER_NODE=8
+export GPUS_PER_NODES=$NPUS_PER_NODE
+#修改为对应主节点IP
+export MASTER_ADDR=90.90.122.117
+
+# 修改为当前节点的通信网卡
+SOCKET_IFNAME="enp189s0f0"
+export GLOO_SOCKET_IFNAME=$SOCKET_IFNAME
+
+
+
+######################
+# GPU相关的环境变量
+# export NCCL_SOCKET_IFNAME=$SOCKET_IFNAME
+
+# export CUDA_DEVICE_MAX_CONNECTIONS=1   # 利于TP+SP
+######################
+
+
+
+######################
+# NPU相关的环境变量
+
+#! HCCL 相关配置
+export HCCL_EXEC_TIMEOUT=7200
+export HCCL_EVENT_TIMEOUT=7200
+export HCCL_CONNECT_TIMEOUT=7200
+export ACL_DEVICE_SYNC_TIMEOUT=7200
+export HCCL_ASYNC_ERROR_HANDLING=0
+export P2P_HCCL_BUFFSIZE=30
+export HCCL_BUFFSIZE=300
+
+export HCCL_SOCKET_IFNAME=$SOCKET_IFNAME
+export TP_SOCKET_IFNAME=$SOCKET_IFNAME   # NPU？
+
+export HCCL_ASYNC_ERROR_HANDLING=0
+export HCCL_EXEC_TIMEOUT=3600
+export HCCL_CONNECT_TIMEOUT=3600
+export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+
+
+export PYTORCH_NPU_ALLOC_CONF="max_split_size_mb:2048"
+export ASCEND_GLOBAL_LOG_LEVEL=3 # 3：error级？0：debug级？
+
+#TASK_QUEUE_ENABLE，下发优化，图模式设置为1，非图模式设置为2。NPU参数？哪个包
+export TASK_QUEUE_ENABLE=2
+
+# 激活cann
+source /usr/local/Ascend/cann/8.2.RC2.B030/ascend-toolkit/set_env.sh
+source /usr/local/Ascend/cann/8.2.RC2.B030/nnal/atb/set_env.sh
+
+######################
+
+#获取当前节点IP
+CURRENT_IP=$(ifconfig $SOCKET_IFNAME | grep -Eo 'inet (addr:)?([0-9]{1,3}\.){3}[0-9]{1,3}' | awk '{print $NF}')
+echo $CURRENT_IP
+if [ "$MASTER_ADDR" = "$CURRENT_IP" ]; then
+  # 主节点启动
+  ray start --head --port 4918 --dashboard-host=$MASTER_ADDR --node-ip-address=$CURRENT_IP --dashboard-port=4919
+
+  while true; do
+      ray_status_output=$(ray status)
+      npu_count=$(echo "$ray_status_output" | grep -oP '(?<=/)\d+\.\d+(?=\s*(NPU|GPU))' | head -n 1)pwd
+      npu_count_int=$(echo "$npu_count" | awk '{print int($1)}')
+      device_count=$((npu_count_int / $NPUS_PER_NODE))
+
+      # 判断 device_count 是否与 NNODES 相等
+      if [ "$device_count" -eq "$NNODES" ]; then
+          echo "Ray cluster is ready with $device_count devices (from $npu_count NPU/GPU resources), starting Python script."
+          ray status
+          bash $DEFAULT_SH
+          break
+      else
+          echo "Waiting for Ray to allocate $NNODES devices. Current device count: $device_count"
+          sleep 5
+      fi
+  done
+else
+  # 子节点尝试往主节点注册ray直到成功
+  while true; do
+      # 尝试连接 Ray 集群
+      ray start --address="$MASTER_ADDR:4918" --node-ip-address=$CURRENT_IP
+
+      # 检查连接是否成功
+      ray status
+      if [ $? -eq 0 ]; then
+          echo "Successfully connected to the Ray cluster!"
+          break
+      else
+          echo "Failed to connect to the Ray cluster. Retrying in 5 seconds..."
+          sleep 5
+      fi
+  done
+fi
+
+echo "start.sh ended on ${CURRENT_IP}"
