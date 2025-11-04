ed4698d6e9e028214605e84ec18a66ec22597dc5
Changsheng Quan <quanchangsheng@huawei.com>
2025-11-04 17:10:58 +0800

    Yarn use_cpu_initialization patch

--- k8s/patch/apply_megatron.sh
+++ k8s/patch/apply_megatron.sh
@@ -3,6 +3,9 @@ set -x
 
 \cp /home/code/verl-gpu/k8s/patch/megatron.patch/0.12.1/Megatron-LM/megatron/core/transformer/multi_token_prediction.py /opt/Megatron-LM/megatron/core/transformer/multi_token_prediction.py
 # \cp /home/code/verl-gpu/k8s/patch/megatron.patch/0.12.1/Megatron-LM/megatron/core/transformer/dot_product_attention.py /opt/Megatron-LM/megatron/core/transformer/dot_product_attention.py
-echo -e "/033[32mApplied megatron MTP done./033[0m"
+echo -e "\033[32mApplied megatron MTP done.\033[0m"
+
+\cp /home/code/verl-gpu/k8s/patch/megatron.patch/0.12.1/Megatron-LM/megatron/core/models/common/embeddings/yarn_rotary_pos_embedding.py /opt/Megatron-LM/megatron/core/models/common/embeddings/yarn_rotary_pos_embedding.py
+echo -e "\033[32mApplied megatron Yarn use_cpu_initialization done.\033[0m"
 
 set +x
