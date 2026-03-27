01bbcf1d26adb556e4f35a0b449b802601d3e2e1
Zhijie Xia <76631942+zhijie-os@users.noreply.github.com>
2026-03-27 10:07:23 +0800

    +MXFP8 support

--- verl/workers/rollout/vllm_rollout/vllm_async_server.py
+++ verl/workers/rollout/vllm_rollout/vllm_async_server.py
@@ -259,7 +259,7 @@ class vLLMHttpServer:
             logger.info("QAT quantization config injected to vLLM async server")
         elif quantization is not None:
             # Handle other quantization methods (fp8, torchao)
-            _SUPPORTED_QUANTIZATION = ["fp8", "torchao"]
+            _SUPPORTED_QUANTIZATION = ["fp8", "torchao", "ascend"]
             if quantization not in _SUPPORTED_QUANTIZATION:
                 raise ValueError(f"Currently only support {_SUPPORTED_QUANTIZATION} quantization, got: {quantization}")
 
